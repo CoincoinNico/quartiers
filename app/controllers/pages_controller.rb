dad512f5f68887c58ad0fbe16b8edb1887800974
Nicolas Tournaud <ntournaud@gmail.com>
2015-07-18 23:28:11 +0200

    Quartiers first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,24 @@
+class PagesController < ApplicationController
+  def home
+  end
+  def artist
+  end
+  def portfolio
+  end
+  def paint
+  end
+  def glass
+  end
+  def workshop
+  end
+  def coach
+  end
+  def events
+  end
+  def inventory
+  end
+  def home
+  end
+  def home
+  end
+end
